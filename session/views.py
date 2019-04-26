5fd6e95fe2667c8e19a9f46418cdd6f43e1f6e97
juanse1080 <juanmarcon1080@gmail.com>
2019-04-26 09:50:21 -0500

    login funcional

--- session/views.py
+++ session/views.py
@@ -9,13 +9,10 @@ def template(request):
     if request.user.is_authenticated():
         return redirect(views.board)
     else:
-        print(request.POST)
         if request.method == 'POST':
             user = auth(request)
             if user is not None:
                 login(request, user)
-                # if request.GET['next']:
-                #     return render(request, request.GET['next'])
                 return redirect(views.board)
             messages.error(request, 'Credenciales incorrectas.')
         return render(request, 'registration/login.html')
@@ -27,13 +24,8 @@ def logout_view(request):
 
 def auth(request):
     login_valid = models.User.objects.get(email=request.POST['email'])
-    print(login_valid)
     pwd_valid = check_password(request.POST['password'], login_valid.password)
     if login_valid and pwd_valid:
-        try:
-            user = models.User.objects.get(email=request.POST['email'])
-            return user
-        except models.User.DoesNotExist:
-            return None
+        return login_valid
     return None
         
